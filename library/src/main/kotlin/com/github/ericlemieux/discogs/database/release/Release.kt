87631dc7df602bd561c8fac2359b1fd554470e6e
Eric Lemieux <eric@lemieuxdev.com>
2020-10-20 21:48:31 -0400

    Set default empty list for release list fields

--- library/src/main/kotlin/com/github/ericlemieux/discogs/database/release/Release.kt
+++ library/src/main/kotlin/com/github/ericlemieux/discogs/database/release/Release.kt
@@ -18,23 +18,23 @@ import java.util.*
 data class Release(
     val title: String?,
     val id: Int?,
-    val artists: List<ArtistSummary>?,
+    val artists: List<ArtistSummary> = listOf(),
     val dataQuality: String?,
     val thumb: String?,
     val community: ReleaseCommunityContributions?,
-    val companies: List<LabelSummary>?,
+    val companies: List<LabelSummary> = listOf(),
     val country: String?,
     val dateAdded: Date?,
     val dateChanged: Date?,
     val estimatedWeight: Int?,
     @SerializedName("extraartists")
-    val extraArtists: List<ArtistSummary>?,
+    val extraArtists: List<ArtistSummary> = listOf(),
     val formatQuality: Int?,
     // TODO: formats
-    val genres: List<String>?,
+    val genres: List<String> = listOf(),
     // TODO: identifiers
-    val images: List<Image>?,
-    val labels: List<LabelSummary>?,
+    val images: List<Image> = listOf(),
+    val labels: List<LabelSummary> = listOf(),
     val lowestPrice: Float?,
     val masterId: Int?,
     val masterUrl: String?,
@@ -45,8 +45,8 @@ data class Release(
     val resourceUrl: String?,
     // TODO: series
     val status: String?,
-    val style: List<String>?,
+    val style: List<String> = listOf(),
     // TODO: tracklist
     val uri: String?,
-    val videos: List<Video>?,
+    val videos: List<Video> = listOf(),
     val year: Int?)
